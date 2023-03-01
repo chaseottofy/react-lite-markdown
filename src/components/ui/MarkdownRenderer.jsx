1b935aa3e2785800c3683577e50eea3d87abf127
otto <ottofy@zohomail.com>
2023-03-01 05:41:11 -0700

    1.02

--- src/components/ui/MarkdownRenderer.jsx
+++ src/components/ui/MarkdownRenderer.jsx
@@ -29,6 +29,7 @@ export default function MarkdownRenderer(
           <textarea
             value={html}
             onChange={(e) => setHtml(e.target.value)}
+            spellCheck="false"
             className="markdown-input"
             placeholder="# MARKDOWN LITE&#10;## Write your markdown here... &#10;&#10;> (this is not traditional markdown).&#10;## Click on ? mark for list of commands.&#10;&#10;### Change layout/copy output w/ icons next to ? mark.&#10;&#10;### Grab & move divider to resize the editor/preview.&#10;&#10;### Download/Upload with top right icons (json)."
           />
