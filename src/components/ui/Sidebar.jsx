f170c14981879e8d8b4067dc3cf3bcd3078c5724
otto <ottofy@zohomail.com>
2023-03-02 01:00:50 -0700

    1.02

--- src/components/ui/Sidebar.jsx
+++ src/components/ui/Sidebar.jsx
@@ -4,62 +4,6 @@ import { AiFillGithub } from "react-icons/ai";
 export default function Sidebar({ sidebarState }) {
   return (
     <div className={sidebarState ? "sidebar show-sidebar" : "sidebar hide-sidebar"}>
-
-      {/* <div className="sidebar-body">
-
-        <div className="example">
-          <h1 className="header1">#</h1>
-          <h1 className="header1">Header 1</h1>
-        </div>
-
-        <div className="example">
-          <h1 className="header2">##</h1>
-          <h1 className="header2">Header 2</h1>
-        </div>
-
-        <div className="example">
-          <h1 className="header3">###</h1>
-          <h1 className="header3">Header 3</h1>
-        </div>
-
-        <div className="example">
-          <p>*bold*</p>
-          <p className="bold">bold</p>
-        </div>
-
-        <div className="example">
-          <p>- list</p>
-          <li className="list-example">list</li>
-        </div>
-
-        <div className="example">
-          <p>[link](https://google.com)</p>
-          <p className="example-link">link</p>
-        </div>
-
-        <div className="example__wrapper">
-          <p>{`{image link}`}</p>
-          <div className="fake-img">img</div>
-        </div>
-
-        <div className="example__wrapper">
-        <span className="example-break1"></span>
-          <span className="example-break"></span>
-        </div>
-
-        <div className="example__wrapper">
-          <p>&gt; blockquote</p>
-          <p className="blockquote">blockquote</p>
-        </div>
-
-        <div className="example__wrapper">
-          <span>&lt;code&gt;hi()&lt;/code&gt;</span>
-          <pre className="pre">
-            <code className="code">hi()</code>
-          </pre>
-        </div>
-
-      </div> */}
       <div className="sidebar-body">
         <table>
           <tbody>
@@ -100,7 +44,7 @@ export default function Sidebar({ sidebarState }) {
                 <p>- list</p>
               </td>
               <td>
-                <li className="list-example">list</li>
+                <p className="list-example">• list</p>
               </td>
             </tr>
             <tr>
